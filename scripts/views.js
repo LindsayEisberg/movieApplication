1655d14669728e96f550a00ca4fa02cef850f946
Lindsay Eisberg <lindsay.eisberg@gmail.com>
2015-02-17 23:40:34 -0500

    CRUD functional.  Styling still in process

--- scripts/views.js
+++ scripts/views.js
@@ -7,10 +7,18 @@ var ListingView = Backbone.View.extend({
     console.log(this.el);
   },
   events: {
-    "click .deletePost": "removeListing"
+    "click .deletePost": "removeListing",
+    'click .editPost': 'showEdit',
+    'click .submitEdit': 'editListing'
+  },
+
+  showEdit: function() {
+    this.$el.find('.editPost').hide();
+    this.$el.find('.submitEdit').addClass('show');
+    this.$el.find('#updatePost').addClass('show');
+    this.$el.find('.content').hide();
   },
   render: function() {
-    console.log(this.el);
     var markup = this.template(this.model.toJSON());
     this.$el.html(markup);
 
@@ -19,6 +27,25 @@ var ListingView = Backbone.View.extend({
   removeListing: function() {
     this.model.destroy();
     this.$el.remove();
+  },
+  editListing: function(e) {
+    e.preventDefault();
+    this.model.set({
+      title: this.$el.find('input[name="editTitle"]').val(),
+      director: this.$el.find('input[name="editDirector"]').val(),
+      synopsis: this.$el.find('textarea[name="editSynopsis"]').val(),
+      poster: this.$el.find('input[name="editPoster"]').val()
+    })
+
+    this.model.save();
+
+
+    $('.content').show();
+    $('.editPost').show();
+    $('#updatePost').removeClass('show');
+    $('.submitEdit').removeClass('show');
+    console.log('submit edit button clicked');
+
   }
 });
 
@@ -31,7 +58,8 @@ var CollectionView = Backbone.View.extend({
     this.addAllListings();
   },
   events: {
-    'submit #createMovie': 'createListing'
+    'submit #createMovie': 'createListing',
+    'click .submitEdit': 'editCollection'
   },
   createListing: function(e) {
     e.preventDefault();
@@ -43,10 +71,19 @@ var CollectionView = Backbone.View.extend({
     };
     var movieListing = new MovieListing(newListing);
     movieListing.save();
+    console.log(this.collection.length);
+    this.collection.add(movieListing);
+    console.log(this.collection.length);
     this.addAllListings();
     $('input').val('');
     $('textarea').val('');
 
+  },
+
+  editCollection: function() {
+    this.$el.find('article').remove();
+    this.addAllListings();
+    console.log('collection back');
   },
 
   addOneListing: function(movie) {
